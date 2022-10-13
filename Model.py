97b6922489685acae029ee96ffce8b81b736c675
GGTTGTGT-2020 <76727642+GGTTGTGT-2020@users.noreply.github.com>
2022-10-13 21:15:57 +0800

    Add files via upload

--- Model.py
+++ Model.py
@@ -1,5 +1,5 @@
 
-from .Model_parts import *
+from .Model_parts_upload import *
 
 class Cascade_UNet(nn.Module):
     """
@@ -12,58 +12,58 @@ class Cascade_UNet(nn.Module):
         self.bilinear = bilinear
         C = 64
         filter_seg = [C, 2 * C, 4 * C, 8 * C, 16 * C]
-        filter_dif = [C / 2, C, 2 * C, 4 * C, 8 * C]
+        filter_dif = [C // 2, C, 2 * C, 4 * C, 8 * C]
         """
         Segmentation model
         """
-        self.in_seg = Double_Conv(n_channels, filter_seg[0])
-        self.down_seg_1 = Down(filter_seg[0], filter_seg[1])
-        self.down_seg_2 = Down(filter_seg[1], filter_seg[2])
-        self.down_seg_3 = Down(filter_seg[2], filter_seg[3])
-        self.down_seg_4 = Down(filter_seg[3], filter_seg[4])
-        self.up_seg_1 = Up(filter_seg[4] + filter_seg[3], filter_seg[3], bilinear = bilinear)
-        self.up_seg_2 = Up(filter_seg[3] + filter_seg[2], filter_seg[2], bilinear = bilinear)
-        self.up_seg_3 = Up(filter_seg[2] + filter_seg[1], filter_seg[1], bilinear = bilinear)
-        self.up_seg_4 = Up(filter_seg[1] + filter_seg[0], filter_seg[0], bilinear = bilinear)
-        self.out_seg = nn.Conv2d(filter_seg[0], 2, kernel_size = 1)
+        self.inc = DoubleConv(n_channels, filter_seg[0])
+        self.down1 = Down(filter_seg[0], filter_seg[1])
+        self.down2 = Down(filter_seg[1], filter_seg[2])
+        self.down3 = Down(filter_seg[2], filter_seg[3])
+        self.down4 = Down(filter_seg[3], filter_seg[4])
+        self.up1 = Up(filter_seg[4] + filter_seg[3], filter_seg[3], bilinear = bilinear)
+        self.up2 = Up(filter_seg[3] + filter_seg[2], filter_seg[2], bilinear = bilinear)
+        self.up3 = Up(filter_seg[2] + filter_seg[1], filter_seg[1], bilinear = bilinear)
+        self.up4 = Up(filter_seg[1] + filter_seg[0], filter_seg[0], bilinear = bilinear)
+        self.outc = OutConv(filter_seg[0], 2)
 
         """
         Differentiation model
         """
-        self.in_dif = Double_Conv(2, filter_dif[0])
-        self.down_dif_1 = Down(filter_dif[0], filter_dif[1])
-        self.down_dif_2 = Down(filter_dif[1], filter_dif[2])
-        self.down_dif_3 = Down(filter_dif[2], filter_dif[3])
-        self.down_dif_4 = Down(filter_dif[3], filter_dif[4])
-        self.up_dif_1 = Up(filter_dif[4] + filter_dif[3], filter_dif[3], bilinear = bilinear)
-        self.up_dif_2 = Up(filter_dif[3] + filter_dif[2], filter_dif[2], bilinear = bilinear)
-        self.up_dif_3 = Up(filter_dif[2] + filter_dif[1], filter_dif[1], bilinear = bilinear)
-        self.up_dif_4 = Up(filter_dif[1] + filter_dif[0], filter_dif[0], bilinear = bilinear)
-        self.out_dif =  nn.Conv2d(filter_dif[0], n_classes, kernel_size = 1)
+        self.inc2 = DoubleConv(2, filter_dif[0])
+        self.down11 = Down(filter_dif[0], filter_dif[1])
+        self.down22 = Down(filter_dif[1], filter_dif[2])
+        self.down33 = Down(filter_dif[2], filter_dif[3])
+        self.down44 = Down(filter_dif[3], filter_dif[4])
+        self.up11 = Up(filter_dif[4] + filter_dif[3], filter_dif[3], bilinear = bilinear)
+        self.up22 = Up(filter_dif[3] + filter_dif[2], filter_dif[2], bilinear = bilinear)
+        self.up33 = Up(filter_dif[2] + filter_dif[1], filter_dif[1], bilinear = bilinear)
+        self.up44 = Up(filter_dif[1] + filter_dif[0], filter_dif[0], bilinear = bilinear)
+        self.outc2 =  OutConv(filter_dif[0], n_classes)
 
     def forward(self, x):
-        x1 = self.in_seg(x)
-        x2 = self.down_seg_1(x1)
-        x3 = self.down_seg_2(x2)
-        x4 = self.down_seg_3(x3)
-        x5 = self.down_seg_4(x4)
-        x_seg = self.up_seg_1(x5, x4)
-        x_seg = self.up_seg_2(x_seg, x3)
-        x_seg = self.up_seg_3(x_seg, x2)
-        x_seg = self.up_seg_4(x_seg, x1)
-        out_seg = self.out_seg(x_seg)
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x_seg = self.up1(x5, x4)
+        x_seg = self.up2(x_seg, x3)
+        x_seg = self.up3(x_seg, x2)
+        x_seg = self.up4(x_seg, x1)
+        out_seg = self.outc(x_seg)
         logit = torch.softmax(out_seg, dim = 1)
         new_x = torch.cat([x,logit[:,1,:,:].unsqueeze(1)], dim = 1)
-        cx1 = self.in_dif(new_x)
-        cx2 = self.down_dif_1(cx1)
-        cx3 = self.down_dif_2(cx2)
-        cx4 = self.down_dif_3(cx3)
-        cx5 = self.down_dif_4(cx4)
-        x_dif = self.up_dif_1(cx5, cx4)
-        x_dif = self.up_dif_2(x_dif, cx3)
-        x_dif = self.up_dif_3(x_dif, cx2)
-        x_dif = self.up_dif_4(x_dif, cx1)
-        out_dif = self.out_dif(x_dif)
+        cx1 = self.inc2(new_x)
+        cx2 = self.down11(cx1)
+        cx3 = self.down22(cx2)
+        cx4 = self.down33(cx3)
+        cx5 = self.down44(cx4)
+        x_dif = self.up11(cx5, cx4)
+        x_dif = self.up22(x_dif, cx3)
+        x_dif = self.up33(x_dif, cx2)
+        x_dif = self.up44(x_dif, cx1)
+        out_dif = self.outc2(x_dif)
         return out_seg, out_dif
 
 
@@ -78,15 +78,15 @@ class Pipleline_UNet_seg(nn.Module):
         self.bilinear = bilinear
         C = 32
         filter_seg = [C, 2 * C, 4 * C, 8 * C, 16 * C]
-        self.inc = Double_Conv(n_channels, 32)
+        self.inc = DoubleConv(n_channels, 32)
         self.down1 = Down(filter_seg[0], filter_seg[1])
         self.down2 = Down(filter_seg[1], filter_seg[2])
         self.down3 = Down(filter_seg[2], filter_seg[3])
         self.down4 = Down(filter_seg[3], filter_seg[4])
-        self.up1 = Up(filter_seg[4] + filter_seg[3], filter_seg[3], bilinear=bilinear)
-        self.up2 = Up(filter_seg[3] + filter_seg[2], filter_seg[2], bilinear=bilinear)
-        self.up3 = Up(filter_seg[2] + filter_seg[1], filter_seg[1], bilinear=bilinear)
-        self.up4 = Up(filter_seg[1] + filter_seg[0], filter_seg[0], bilinear=bilinear)
+        self.up1 = Up(filter_seg[4] + filter_seg[3], filter_seg[3], bilinear = bilinear)
+        self.up2 = Up(filter_seg[3] + filter_seg[2], filter_seg[2], bilinear = bilinear)
+        self.up3 = Up(filter_seg[2] + filter_seg[1], filter_seg[1], bilinear = bilinear)
+        self.up4 = Up(filter_seg[1] + filter_seg[0], filter_seg[0], bilinear = bilinear)
         self.outc = nn.Conv2d(32, n_classes, kernel_size=1)
 
     def forward(self, x):
@@ -115,7 +115,7 @@ class Pipleline_UNet_dif(nn.Module):
         self.bilinear = bilinear
         C = 32
         filter_dif = [C, 2 * C, 4 * C, 8 * C, 16 * C]
-        self.inc = Double_Conv(n_channels, 32)
+        self.inc = DoubleConv(n_channels, 32)
         self.down1 = Down(filter_dif[0], filter_dif[1])
         self.down2 = Down(filter_dif[1], filter_dif[2])
         self.down3 = Down(filter_dif[2], filter_dif[3])
@@ -151,7 +151,7 @@ class Separate_UNet(nn.Module):
         self.bilinear = bilinear
         C = 32
         filter_seg = [C, 2 * C, 4 * C, 8 * C, 16 * C]
-        self.inc = Double_Conv(n_channels, 32)
+        self.inc = DoubleConv(n_channels, 32)
         self.down1 = Down(filter_seg[0], filter_seg[1])
         self.down2 = Down(filter_seg[1], filter_seg[2])
         self.down3 = Down(filter_seg[2], filter_seg[3])
